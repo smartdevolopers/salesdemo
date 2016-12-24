cb6469accd91648ef78e3b665ed4f05e0e98869e
Dell <Dell@192.168.0.25>
2016-12-23 16:51:36 -0800

    Initial commit of SalesDemo

---
+++ SalesDemo/src/com/dit/domain/MainTest.java
@@ -0,0 +1,17 @@
+package com.dit.domain;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		Sales sale = new Sales();
+		
+		sale.salesRecords();
+		display();
+		
+	}
+	
+	public static void display(){
+		System.out.println("Display data....");
+	}
+
+}
---
+++ SalesDemo/src/com/dit/domain/Sales.java
@@ -0,0 +1,10 @@
+package com.dit.domain;
+
+public class Sales {
+	
+	public void salesRecords(){
+		System.out.println("Sales records.....");
+	}
+	
+	
+}

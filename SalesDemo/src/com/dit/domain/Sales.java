1d067fade272add44eba7a3d33915bfb320c72a1
Dell <Dell@192.168.0.25>
2016-12-23 20:29:20 -0800

    partially implemented in display data from array.

--- SalesDemo/src/com/dit/domain/Sales.java
+++ SalesDemo/src/com/dit/domain/Sales.java
@@ -2,8 +2,16 @@ package com.dit.domain;
 
 public class Sales {
 	
+	int data[] = {8, 19};
+	
 	public void salesRecords(){
 		System.out.println("Sales records.....");
+		System.out.println("Data");
+		System.out.println("-------");
+		
+		for(int i = 0; i<data.length; i++){
+			System.out.println((i+1)+"st value "+data[i]);
+		}
 	}
 	
 	
